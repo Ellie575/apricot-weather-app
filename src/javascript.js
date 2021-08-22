6c2833950e5a32250e4c5578d6f812aae613eaa7
Ellie575 <elliekubo@hotmail.com>
2021-08-22 18:24:38 +1000

    Showring the current temperature

--- src/javascript.js
+++ src/javascript.js
@@ -1,1 +1,17 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  console.log(response.data);
+  console.log(response.data.name);
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  let cityH2 = document.querySelector("#city");
+  cityH2.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "56e0818fd22a5efa4a6c31eab2ac96a0";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
