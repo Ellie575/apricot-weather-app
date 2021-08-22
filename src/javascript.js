55ccc40381adf5434a2def709546c6d92be5f39b
Ellie575 <elliekubo@hotmail.com>
2021-08-22 21:16:52 +1000

    Added search engine

--- src/javascript.js
+++ src/javascript.js
@@ -39,7 +39,17 @@ function displayTemperature(response) {
   );
 }
 
-let apiKey = "56e0818fd22a5efa4a6c31eab2ac96a0";
-let city = "Paris";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "56e0818fd22a5efa4a6c31eab2ac96a0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  search(cityInput.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
